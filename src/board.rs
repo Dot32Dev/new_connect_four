a3d9f41129cf10c7fdf6bf90a1a39f6c5be5d77d
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-12-15 19:02:44 +0800

    Vertical and horizontal win checks

--- src/board.rs
+++ src/board.rs
@@ -1,66 +1,127 @@
+use std::cmp;
+use crate::Colour;
 use colored::*;
 
-#[derive(Clone, Copy)]
-pub enum Piece {
-	None,
-	Red,
-	Blue
-}
-
-
 pub struct Board {
-	pub board: [[Piece; 6]; 7],
-	pub highlights: [[bool; 6]; 7],
+    pub board: [[Colour; 6]; 7],
+    pub highlights: [[bool; 6]; 7],
 }
 
 impl Board {
-	pub fn new() -> Board {
-			Board {
-					board: [[Piece::None; 6]; 7],
-					highlights: [[false; 6]; 7],
-			}
+    pub fn new() -> Board {
+        Board {
+            board: [[Colour::None; 6]; 7],
+            highlights: [[false; 6]; 7],
+        }
+    }
+
+    pub fn print(&self) {
+        println!(" ╭ {} ╮", "1  2  3  4  5  6  7");
+        for y in 0..6 {
+            print!(" │");
+            for x in 0..7 {
+                match self.board[x][y] {
+                    Colour::None => print!("{}", " ▿ ".dimmed()),
+                    Colour::Red => match self.highlights[x][y] {
+                        true => print!("{}", " ◈ ".red()),
+                        false => print!("{}", " ◉ ".red()),
+                    },
+                    Colour::Blue => match self.highlights[x][y] {
+                        true => print!("{}", " ◈ ".blue()),
+                        false => print!("{}", " ◉ ".blue()),
+                    },
+                }
+            }
+            print!("│");
+            println!();
+        }
+        println!(" ╰─────────────────────╯");
+    }
+
+    pub fn drop_piece(&mut self, x: usize, piece: Colour) {
+        for y in (0..6).rev() {
+            match self.board[x][y] {
+                Colour::None => {
+                    self.board[x][y] = piece;
+                    break;
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    pub fn highlight_piece(&mut self, x: usize, y: usize) {
+        self.highlights[x][y] = true;
+    }
+
+	pub fn unhighlight_board(&mut self) {
+		self.highlights = [[false; 6]; 7];
 	}
 
-	pub fn print(&self) {
-			println!(" ╭ {} ╮", "1  2  3  4  5  6  7");
-			for y in 0..6 {
-					print!(" │");
-					for x in 0..7 {
-							match self.board[x][y] {
-									Piece::None => print!("{}", " ▿ ".dimmed()),
-									Piece::Red => {
-											match self.highlights[x][y] {
-													true => print!("{}", " ◈ ".red()),
-													false => print!("{}", " ◉ ".red())
-											}
-									},
-									Piece::Blue => {
-											match self.highlights[x][y] {
-													true => print!("{}", " ◈ ".blue()),
-													false => print!("{}", " ◉ ".blue())
-											}
-									},
-							}
-					}
-					print!("│");
-					println!();
-			}
-			println!(" ╰─────────────────────╯");
-	}
+	pub fn check_win_at(&mut self, x: usize, y: usize) -> Option<Colour> {
+		let colour = self.board[x][y];
+		println!("Colour: {:?}", colour);
 
-	pub fn drop_piece(&mut self, x: usize, piece: Piece) {
-			for y in (0..6).rev() {
-					match self.board[x][y] {
-							Piece::None => {
-									self.board[x][y] = piece;
-									break;
-							},
-							_ => continue,
-					}
+		// VERTICAL
+		if y < 3 {
+			for i in y..=y+3 {
+				match self.board[x][i] {
+					// Match Guard (https://doc.rust-lang.org/reference/expressions/match-expr.html#match-guards)
+					col if col == colour => {
+						if i == y+3 {
+							return Some(colour);
+						}
+					},
+					_ => break,
+				}
 			}
-	}
+		}
 
-	pub fn highlight_piece(&mut self, x: usize, y: usize) {
-			self.highlights[x][y] = true;
+		// HORIZONTAL
+		let mut count = 0;
+		if x < 7 - 3 {
+			for i in cmp::max(x as i32 - 3, 0) as usize..cmp::min(x as i32 + 3, 7 - 1)  as usize {
+				match self.board[i][y] {
+					col if col == colour => {
+						count += 1;
+						if count == 4 {
+							return Some(colour);
+						}
+					},
+					_ => count = 0,
+				}
+			}
+		}
+
+		// DIAGONAL ╱
+		// if y < 3 && x < 7 - 3 {
+			// for i in (y..=y+3).rev() {
+			// 	println!("x: {}, y: {}", x as i32 - (y as i32-i as i32), i);
+			// 	match self.board[(x as i32 - (y as i32-i as i32)) as usize][i] {
+			// 		col if col == colour => {
+			// 			if i == y {
+			// 				return Some(colour);
+			// 			}
+			// 		},
+			// 		_ => break,
+			// 	}
+			// }
+		// }
+
+		// DIAGONAL ╱
+		// let mut count = 0;
+		// for i in cmp::max(y as i32 - 3, 0) as usize..cmp::min(y as i32 + 3, 7 - 1)  as usize {
+		// 	match self.board[(x as i32 + i as i32 -y as i32) as usize][(y as i32 + i as i32 -y as i32) as usize] {
+		// 		col if col == colour => {
+		// 			count += 1;
+		// 			if count == 4 {
+		// 				return Some(colour);
+		// 			}
+		// 		},
+		// 		_ => count = 0,
+		// 	}
+		// }
+
+		None
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
 mod board;
-use colored::*;
 use board::*;
+use colored::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Colour {
+    None,
+    Red,
+    Blue,
+}
 
 fn main() {
     let mut board = Board::new();
 
-    board.drop_piece(4, Piece::Red);
-    board.drop_piece(4, Piece::Blue);
-    board.drop_piece(5, Piece::Blue);
+    board.drop_piece(4, Colour::Red);
+    board.drop_piece(4, Colour::Blue);
 
-    board.highlight_piece(5, 5);
+    board.drop_piece(5, Colour::Blue);
+    board.drop_piece(5, Colour::Blue);
+    board.drop_piece(5, Colour::Red);
+    board.drop_piece(5, Colour::Blue);
+
+    board.drop_piece(1, Colour::Red);
+    board.drop_piece(2, Colour::Blue);
+    board.drop_piece(3, Colour::Red);
+
+    board.drop_piece(3, Colour::Blue);
+    board.drop_piece(4, Colour::Blue);
+
+    board.drop_piece(2, Colour::Red);
+    board.drop_piece(1, Colour::Blue);
+    board.drop_piece(1, Colour::Red);
+
+    board.drop_piece(0, Colour::Blue);
+    board.drop_piece(0, Colour::Red);
+    board.drop_piece(0, Colour::Blue);
+    board.drop_piece(0, Colour::Red);
+
+    board.highlight_piece(5, 2);
+    board.highlight_piece(3, 5);
+
+    println!("{:?}", board.check_win_at(5, 2));
+    println!("{:?}", board.check_win_at(0, 2));
+    // println!("{:?}", board.check_win_at(3, 5));
 
     println!("\n    It's {}'s turn!\n", "Red".red());
     board.print();
