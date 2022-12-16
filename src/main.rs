25a92c1a5522546fc4defa08157c3cca6f490457
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-12-16 11:57:02 +0800

    Clean main gameplay loop

--- src/main.rs
+++ src/main.rs
@@ -11,78 +11,30 @@ pub enum Colour {
 
 fn main() {
     let mut board = Board::new();
+    let mut turn = Colour::Red;
 
-    // board.drop_piece(4, Colour::Red);
-    // board.drop_piece(4, Colour::Blue);
-
-    // board.drop_piece(5, Colour::Blue);
-    // board.drop_piece(5, Colour::Blue);
-    // board.drop_piece(5, Colour::Red);
-    // board.drop_piece(5, Colour::Blue);
-
-    // board.drop_piece(1, Colour::Red);
-    // board.drop_piece(2, Colour::Blue);
-    // board.drop_piece(3, Colour::Red);
-
-    // board.drop_piece(3, Colour::Blue);
-    // board.drop_piece(4, Colour::Blue);
-
-    // board.drop_piece(2, Colour::Red);
-    // board.drop_piece(1, Colour::Blue);
-    // board.drop_piece(1, Colour::Red);
-
-    // board.drop_piece(0, Colour::Blue);
-    // board.drop_piece(0, Colour::Red);
-    // board.drop_piece(0, Colour::Blue);
-    // board.drop_piece(0, Colour::Red);
-
-    // board.highlight_piece(5, 2);
-    // board.highlight_piece(3, 5);
-
-    // println!("{:?}", board.check_win_at(5, 2));
-    // println!("{:?}", board.check_win_at(0, 2));
-    // // println!("{:?}", board.check_win_at(3, 5));
-
-    // println!("\n    It's {}'s turn!\n", "Red".red());
-    // board.print();
     loop {
-        println!("\n    It's {}'s turn!\n", "Red".red());
+        println!("\n    It's {}'s turn!\n", if turn == Colour::Red { "Red".red() } else { "Blue".blue() });
         board.print();
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let input: usize = input.trim().parse().unwrap();
-        board.drop_piece(input -1, Colour::Red);
-        match board.check_win_at(input -1) {
-            Some(Colour::Red) => {
-                println!("\n    {} wins!\n", "Red".red());
-                board.print();
-                break;
+        let input: usize = match input.trim().parse() {
+            Ok(num) if num <= 7 => num,
+            _ => {
+                println!("{}", ">> Please select a valid column".yellow());
+                continue;
             }
-            Some(Colour::Blue) => {
-                println!("\n    {} wins!\n", "Blue".blue());
+        };
+        board.drop_piece(input -1, turn);
+        match board.check_win_at(input -1) {
+            Some(colour) => {
+                println!("\n    {} wins!\n", if colour == Colour::Red { "Red".red() } else { "Blue".blue() });
                 board.print();
                 break;
             }
             _ => (),
         }
-        println!("\n    It's {}'s turn!\n", "Blue".blue());
-        board.print();
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let input: usize = input.trim().parse().unwrap();
-        board.drop_piece(input -1, Colour::Blue);
-        match board.check_win_at(input -1) {
-            Some(Colour::Red) => {
-                println!("\n    {} wins!\n", "Red".red());
-                board.print();
-                break;
-            }
-            Some(Colour::Blue) => {
-                println!("\n    {} wins!\n", "Blue".blue());
-                board.print();
-                break;
-            }
-            _ => (),
-        }
+
+        turn = if turn == Colour::Red { Colour::Blue } else { Colour::Red };
     }
 }
