8fcdc57777f2a0cee07bb797daf077130495dbb6
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-08-31 14:39:44 +0800

    Run `cargo fmt` to fix mixed indentation

--- src/board.rs
+++ src/board.rs
@@ -1,175 +1,193 @@
-use std::cmp;
 use crate::Colour;
 use colored::*;
+use std::cmp;
 
 #[derive(Clone, Copy)]
 pub struct Board {
     pub board: [[Colour; 6]; 7],
-	pub highlighted_column: Option<usize>,
+    pub highlighted_column: Option<usize>,
 }
 
 impl Board {
     pub fn new() -> Board {
         Board {
             board: [[Colour::None; 6]; 7],
-			highlighted_column: None,
+            highlighted_column: None,
         }
     }
 
-	// I would use this if i weren't using Termion
-	// This is just a wrapper for the to_string() function, that prints the string.
-	#[allow(dead_code)]
+    // I would use this if I weren't using Termion
+    // This is just a wrapper for the to_string() function, that prints the string.
+    #[allow(dead_code)]
     pub fn print(&self) {
         println!("{}", self.to_string());
     }
 
-	pub fn to_string(&self) -> String {
-		let mut string = String::new();
-		string.push_str("╭ 1  2  3  4  5  6  7 ╮\n");
-		for y in 0..6 {
-			string.push_str("│");
-			for x in 0..7 {
-				let border = match self.highlighted_column {
-					Some(column) => if column == x { "┊" } else { " " },
-					None => " ",
-				};
-				match self.board[x][y] {
-					Colour::None => string.push_str(&format!("{1}{0}{1}", "▿".dimmed(), border)),
-					Colour::Red => string.push_str(&format!("{1}{0}{1}", "◉".red(), border)),
-					Colour::Blue => string.push_str(&format!("{1}{0}{1}", "◉".blue(), border)),
-				}
-			}
-			string.push_str("│\n");
-		}
-		string.push_str("╰");
-		for x in 0..7 {
-			match self.highlighted_column {
-				Some(column) => if column == x {
-					string.push_str("┴─┴")
-				} else {
-					string.push_str("───")
-				},
-				None => string.push_str("───"),
-			}
-		}
-		string.push_str("╯");
-		string
-	}
+    pub fn to_string(&self) -> String {
+        let mut string = String::new();
+        string.push_str("╭ 1  2  3  4  5  6  7 ╮\n");
+        for y in 0..6 {
+            string.push_str("│");
+            for x in 0..7 {
+                let border = match self.highlighted_column {
+                    Some(column) => {
+                        if column == x {
+                            "┊"
+                        } else {
+                            " "
+                        }
+                    }
+                    None => " ",
+                };
+                match self.board[x][y] {
+                    Colour::None => string.push_str(&format!("{1}{0}{1}", "▿".dimmed(), border)),
+                    Colour::Red => string.push_str(&format!("{1}{0}{1}", "◉".red(), border)),
+                    Colour::Blue => string.push_str(&format!("{1}{0}{1}", "◉".blue(), border)),
+                }
+            }
+            string.push_str("│\n");
+        }
+        string.push_str("╰");
+        for x in 0..7 {
+            match self.highlighted_column {
+                Some(column) => {
+                    if column == x {
+                        string.push_str("┴─┴")
+                    } else {
+                        string.push_str("───")
+                    }
+                }
+                None => string.push_str("───"),
+            }
+        }
+        string.push_str("╯");
+        string
+    }
 
     pub fn drop_piece(&mut self, x: usize, piece: Colour) -> Result<usize, &str> {
         for y in (0..6).rev() {
             match self.board[x][y] {
                 Colour::None => {
                     self.board[x][y] = piece;
-					self.highlighted_column = Some(x);
-					return Ok(y); // Return Ok if the piece was dropped, and the y position of the piece
+                    self.highlighted_column = Some(x);
+                    return Ok(y); // Return Ok if the piece was dropped, and the y position of the piece
                 }
                 _ => continue,
             }
         }
-		Err("Column is full") // Return Err if the column is full
+        Err("Column is full") // Return Err if the column is full
     }
 
-	// This took so long to write, but it works! (I think)
-	pub fn check_win_at(&mut self, x: usize) -> Option<Colour> {
-		// Calculate the y position of the piece
-		let mut y = 0;
-		for i in 0..6 {
-			if !(self.board[x][i] == Colour::None) {
-				y = i;
-				break;
-			}
-		}
+    // This took so long to write, but it works! (I think)
+    pub fn check_win_at(&mut self, x: usize) -> Option<Colour> {
+        // Calculate the y position of the piece
+        let mut y = 0;
+        for i in 0..6 {
+            if !(self.board[x][i] == Colour::None) {
+                y = i;
+                break;
+            }
+        }
 
-		// println!("x: {}, y: {}", x, y);
+        // println!("x: {}, y: {}", x, y);
 
-		let colour = self.board[x][y];
-		// println!("Colour: {:?}", colour);
+        let colour = self.board[x][y];
+        // println!("Colour: {:?}", colour);
 
-		// VERTICAL
-		if y < 3 {
-			for i in y..=y+3 {
-				match self.board[x][i] {
-					// Match Guard (https://doc.rust-lang.org/reference/expressions/match-expr.html#match-guards)
-					col if col == colour => {
-						if i == y+3 {
-							return Some(colour);
-						}
-					},
-					_ => break,
-				}
-			}
-		}
+        // VERTICAL
+        if y < 3 {
+            for i in y..=y + 3 {
+                match self.board[x][i] {
+                    // Match Guard (https://doc.rust-lang.org/reference/expressions/match-expr.html#match-guards)
+                    col if col == colour => {
+                        if i == y + 3 {
+                            return Some(colour);
+                        }
+                    }
+                    _ => break,
+                }
+            }
+        }
 
-		// HORIZONTAL
-		let mut count = 0;
-		for i in cmp::max(x as i32 - 3, 0) as usize..cmp::min(x as i32 + 3, 7 - 1)  as usize {
-			match self.board[i][y] {
-				col if col == colour => {
-					count += 1;
-					if count == 4 {
-						return Some(colour);
-					}
-				},
-				_ => count = 0,
-			}
-		}
+        // HORIZONTAL
+        let mut count = 0;
+        for i in cmp::max(x as i32 - 3, 0) as usize..cmp::min(x as i32 + 3, 7 - 1) as usize {
+            match self.board[i][y] {
+                col if col == colour => {
+                    count += 1;
+                    if count == 4 {
+                        return Some(colour);
+                    }
+                }
+                _ => count = 0,
+            }
+        }
 
-		// DIAGONAL ╱
-		let mut count = 0;
-		for i in (cmp::max(y as i8-3 as i8, 0) as usize..=y+3).rev() {
-			// println!("x: {}, y: {}", x as i32 - (y as i32-i as i32), i);
-			if cmp::max(x as i32 - (y as i32-i as i32), 0) == x as i32 - (y as i32-i as i32) && cmp::min(i, 6-1) == i {
-				match self.board[cmp::min(cmp::max(x as i32 - (y as i32-i as i32), 0), 7-1) as usize][cmp::min(i, 6-1)] {
-					col if col == colour => {
-						count += 1;
-						if count == 4 {
-							return Some(colour);
-						}
-					},
-					_ => count = 0,
-				}
-			}
-		}
+        // DIAGONAL ╱
+        let mut count = 0;
+        for i in (cmp::max(y as i8 - 3 as i8, 0) as usize..=y + 3).rev() {
+            // println!("x: {}, y: {}", x as i32 - (y as i32-i as i32), i);
+            if cmp::max(x as i32 - (y as i32 - i as i32), 0) == x as i32 - (y as i32 - i as i32)
+                && cmp::min(i, 6 - 1) == i
+            {
+                match self.board
+                    [cmp::min(cmp::max(x as i32 - (y as i32 - i as i32), 0), 7 - 1) as usize]
+                    [cmp::min(i, 6 - 1)]
+                {
+                    col if col == colour => {
+                        count += 1;
+                        if count == 4 {
+                            return Some(colour);
+                        }
+                    }
+                    _ => count = 0,
+                }
+            }
+        }
 
-		// DIAGONAL ╲
-		let mut count = 0;
-		for i in (cmp::max(y as i8-3 as i8, 0) as usize..=y+3).rev() {
-			if cmp::max(x as i32 + (y as i32-i as i32), 0) == x as i32 + (y as i32-i as i32) && cmp::min(i, 6-1) == i {
-				match self.board[cmp::min(cmp::max(x as i32 + (y as i32-i as i32), 0), 7-1) as usize][cmp::min(i, 6-1)] {
-					col if col == colour => {
-						count += 1;
-						if count == 4 {
-							return Some(colour);
-						}
-					},
-					_ => count = 0,
-				}
-			}
-		}
+        // DIAGONAL ╲
+        let mut count = 0;
+        for i in (cmp::max(y as i8 - 3 as i8, 0) as usize..=y + 3).rev() {
+            if cmp::max(x as i32 + (y as i32 - i as i32), 0) == x as i32 + (y as i32 - i as i32)
+                && cmp::min(i, 6 - 1) == i
+            {
+                match self.board
+                    [cmp::min(cmp::max(x as i32 + (y as i32 - i as i32), 0), 7 - 1) as usize]
+                    [cmp::min(i, 6 - 1)]
+                {
+                    col if col == colour => {
+                        count += 1;
+                        if count == 4 {
+                            return Some(colour);
+                        }
+                    }
+                    _ => count = 0,
+                }
+            }
+        }
 
-		None
-	}
+        None
+    }
 
-	// Used for animating the drop of a piece
-	pub fn animation_frame(&mut self, frame: usize) -> Board {
-		// Calculate the y position of the piece
-		let mut y = 0;
-		for i in 0..6 {
-			if !(self.board[self.highlighted_column.unwrap()][i] == Colour::None) {
-				y = i;
-				break;
-			}
-		}
-		// Finds the colour of the piece
-		let colour = self.board[self.highlighted_column.unwrap()][y];
-		// Creates a new board based off the current board
-		let mut board = self.clone();
-		// Removes the piece from the new board
-		board.board[self.highlighted_column.unwrap()][y] = Colour::None;
-		// Adds the piece to the new board, at the Y index specified by frame
-		board.board[self.highlighted_column.unwrap()][frame] = colour;
-		// Returns the new board
-		board
-	}
+    // Used for animating the drop of a piece
+    pub fn animation_frame(&mut self, frame: usize) -> Board {
+        // Calculate the y position of the piece
+        let mut y = 0;
+        for i in 0..6 {
+            if !(self.board[self.highlighted_column.unwrap()][i] == Colour::None) {
+                y = i;
+                break;
+            }
+        }
+        // Finds the colour of the piece
+        let colour = self.board[self.highlighted_column.unwrap()][y];
+        // Creates a new board based off the current board
+        let mut board = self.clone();
+        // Removes the piece from the new board
+        board.board[self.highlighted_column.unwrap()][y] = Colour::None;
+        // Adds the piece to the new board, at the Y index specified by frame
+        board.board[self.highlighted_column.unwrap()][frame] = colour;
+        // Returns the new board
+        board
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use board::*;
 use colored::*;
 use unicode_segmentation::UnicodeSegmentation;
 
+use std::io::{stdin, stdout, Read, Write};
 use termion::raw::IntoRawMode;
-use termion::terminal_size;
 use termion::screen::IntoAlternateScreen;
-use std::io::{Read, Write, stdout, stdin};
+use termion::terminal_size;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Colour {
@@ -18,7 +18,13 @@ pub enum Colour {
 fn main() {
     // For handling Termion and it's input
     let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap().into_alternate_screen().unwrap();
+    let mut stdout = stdout
+        .lock()
+        .into_raw_mode()
+        .unwrap()
+        .into_alternate_screen()
+        .unwrap();
+
     let stdin = stdin();
     let stdin = stdin.lock();
     let mut bytes = stdin.bytes();
@@ -31,57 +37,55 @@ fn main() {
     let mut turns = 0;
     // Game loop
     while turns < 42 { // 42 is the maximum number of turns, as the board is 7x6
-        // This just redraws the board, 
-        // but with the current turn, the highlighted column, etc as well. 
-        // It saves me from having to write the same code repeatedly.
+        // Refreshes the board, current turn, highlighted column, etc
         redraw_game(&mut stdout, &board, turn);
 
         // This checks for input and updates the board
         loop {
             let b = bytes.next().unwrap().unwrap();
             match b {
-                    // Quit on Ctrl+C
-                    3 => return,
-                    // Enter or space key
-                    13 | 32 => if input != 8 {break},
-                    // 1-7
-                    49..=55 => {
-                        input = (b - 49) as usize;
-                        board.highlighted_column = Some(input);
-                        redraw_game(&mut stdout, &board, turn);
-                    },
-                    // Left arrow
-                    68 => {
-                        match board.highlighted_column {
-                            Some(column) => {
-                                if column > 0 {
-                                    input = column - 1;
-                                    board.highlighted_column = Some(column - 1);
-                                }
-                            },
-                            None => (),
-                        }
-                        redraw_game(&mut stdout, &board, turn);
-                    },
-                    // Right arrow
-                    67 => {
-                        match board.highlighted_column {
-                            Some(column) => {
-                                if column < 6 {
-                                    input = column + 1;
-                                    board.highlighted_column = Some(column + 1);
-                                }
-                            },
-                            None => (),
-                        }
-                        redraw_game(&mut stdout, &board, turn);
-                    },
-                    // Deselect board if random key pressed
-                    _ => {
-                        board.highlighted_column = None;
-                        redraw_game(&mut stdout, &board, turn);
-                    },
+                // Quit on Ctrl+C
+                3 => return,
+                // Enter or space key
+                13 | 32 => if input != 8 {break;}
+                // 1-7
+                49..=55 => {
+                    input = (b - 49) as usize;
+                    board.highlighted_column = Some(input);
+                    redraw_game(&mut stdout, &board, turn);
                 }
+                // Left arrow
+                68 => {
+                    match board.highlighted_column {
+                        Some(column) => {
+                            if column > 0 {
+                                input = column - 1;
+                                board.highlighted_column = Some(column - 1);
+                            }
+                        }
+                        None => (),
+                    }
+                    redraw_game(&mut stdout, &board, turn);
+                }
+                // Right arrow
+                67 => {
+                    match board.highlighted_column {
+                        Some(column) => {
+                            if column < 6 {
+                                input = column + 1;
+                                board.highlighted_column = Some(column + 1);
+                            }
+                        }
+                        None => (),
+                    }
+                    redraw_game(&mut stdout, &board, turn);
+                }
+                // Deselect board if random key pressed
+                _ => {
+                    board.highlighted_column = None;
+                    redraw_game(&mut stdout, &board, turn);
+                }
+            }
 
             stdout.flush().unwrap();
         }
@@ -92,82 +96,185 @@ fn main() {
                     redraw_game(&mut stdout, &board.animation_frame(frame), turn);
                     std::thread::sleep(std::time::Duration::from_millis(15));
                 }
-            },
+            }
             // If the column is full, just continue the game loop, and don't change turns
             Err(_) => continue,
         }
         match board.check_win_at(input) {
             Some(colour) => {
                 // Clears the screen and hides the cursor
-                centred_print(&mut stdout, &format!("{}{}", termion::clear::All, termion::cursor::Hide), None, 1);
+                centred_print(
+                    &mut stdout,
+                    &format!("{}{}", termion::clear::All, termion::cursor::Hide),
+                    None,
+                    1,
+                );
 
-                centred_print(&mut stdout, &format!("{} wins!", if colour == Colour::Red { "Red".red() } else { "Blue".blue() }), Some(4), size.1/2-6);
-                centred_print(&mut stdout, &format!("{}", board.to_string()), Some(11), size.1/2-4);
-                centred_print(&mut stdout, &format!("{}{}", "Press any key to quit.".dimmed(), termion::cursor::Goto(0, size.1)), Some(10), size.1/2+5);
+                centred_print(
+                    &mut stdout,
+                    &format!(
+                        "{} wins!",
+                        if colour == Colour::Red {
+                            "Red".red()
+                        } else {
+                            "Blue".blue()
+                        }
+                    ),
+                    Some(4),
+                    size.1 / 2 - 6,
+                );
+                centred_print(
+                    &mut stdout,
+                    &format!("{}", board.to_string()),
+                    Some(11),
+                    size.1 / 2 - 4,
+                );
+                centred_print(
+                    &mut stdout,
+                    &format!(
+                        "{}{}",
+                        "Press any key to quit.".dimmed(),
+                        termion::cursor::Goto(0, size.1)
+                    ),
+                    Some(10),
+                    size.1 / 2 + 5,
+                );
                 let _ = bytes.next();
                 return;
             }
             _ => (),
         }
-        turn = if turn == Colour::Red { Colour::Blue } else { Colour::Red }; // Switch turns
+        turn = if turn == Colour::Red {
+            Colour::Blue
+        } else {
+            Colour::Red
+        }; // Switch turns
         turns += 1;
     }
     // Draw condition
     // Only runs if the game loop ends without a winner
     centred_print(&mut stdout, &format!("{}{}", termion::clear::All, termion::cursor::Hide), None, 1);
-    centred_print(&mut stdout, &format!("{}{}", "It's a draw!".dimmed(), termion::cursor::Goto(0, size.1)), Some(6), size.1/2-6);
-    centred_print(&mut stdout, &format!("{}", board.to_string()), Some(11), size.1/2-4);
-    centred_print(&mut stdout, &format!("{}{}", "Press any key to quit.".dimmed(), termion::cursor::Goto(0, size.1)), Some(10), size.1/2+5);
+    
+    centred_print(
+        &mut stdout,
+        &format!(
+            "{}{}",
+            "It's a draw!".dimmed(),
+            termion::cursor::Goto(0, size.1)
+        ),
+        Some(6),
+        size.1 / 2 - 6,
+    );
+    centred_print(
+        &mut stdout,
+        &format!("{}", board.to_string()),
+        Some(11),
+        size.1 / 2 - 4,
+    );
+    centred_print(
+        &mut stdout,
+        &format!(
+            "{}{}",
+            "Press any key to quit.".dimmed(),
+            termion::cursor::Goto(0, size.1)
+        ),
+        Some(10),
+        size.1 / 2 + 5,
+    );
     let _ = bytes.next();
 }
 
 fn centred_print(
-    stdout: &mut termion::raw::RawTerminal<std::io::StdoutLock>, 
+    stdout: &mut termion::raw::RawTerminal<std::io::StdoutLock>,
     strings: &str,
     x: Option<u16>,
     y: u16,
 ) {
     let size = terminal_size().unwrap();
 
-    // Splits the string by newlines and prints each line centred. 
+    // Splits the string by newlines and prints each line centred.
     for (i, string) in strings.lines().enumerate() {
-        write!(stdout,
+        write!(
+            stdout,
             "{}{}",
             // If an x value is provided, use that, otherwise, centre the text
             // This is because using colored's methods add a number of invisable characters, making centering invalid.
             // This is a workaround for that.
             match x {
-                Some(x) => termion::cursor::Goto(size.0/2 - x, y + i as u16),
-                None => termion::cursor::Goto(size.0/2 - (string.graphemes(true).count()/2) as u16, y + i as u16),
+                Some(x) => termion::cursor::Goto(size.0 / 2 - x, y + i as u16),
+                None => termion::cursor::Goto(
+                    size.0 / 2 - (string.graphemes(true).count() / 2) as u16,
+                    y + i as u16
+                ),
             },
-            string,)
-             .unwrap();
+            string,
+        )
+        .unwrap();
         stdout.flush().unwrap();
     }
 }
 
 fn redraw_game(
-    stdout: &mut termion::raw::RawTerminal<std::io::StdoutLock>, 
+    stdout: &mut termion::raw::RawTerminal<std::io::StdoutLock>,
     board: &Board,
     turn: Colour,
 ) {
     let size = terminal_size().unwrap();
 
     // Clears the screen and hides the cursor
-    centred_print(stdout, &format!("{}{}", termion::clear::All, termion::cursor::Hide), None, 1);   
+    centred_print(stdout, &format!("{}{}", termion::clear::All, termion::cursor::Hide), None, 1);
 
-    centred_print(stdout, &format!("It's {}'s turn!", if turn == Colour::Red { "Red".red() } else { "Blue".blue() }), Some(8), size.1/2-6);
-    centred_print(stdout, &format!("{}", board.to_string()), Some(11), size.1/2-4);
+    centred_print(
+        stdout,
+        &format!(
+            "It's {}'s turn!",
+            if turn == Colour::Red {
+                "Red".red()
+            } else {
+                "Blue".blue()
+            }
+        ),
+        Some(8),
+        size.1 / 2 - 6,
+    );
+    centred_print(
+        stdout,
+        &format!("{}", board.to_string()),
+        Some(11),
+        size.1 / 2 - 4,
+    );
 
     // If a column is selected, print the number of the column
-    centred_print(stdout,  &format!("Select column: {}", match board.highlighted_column {
-        Some(column) => format!("{}", (column + 1).to_string().yellow()),
-        None => "".to_string(),
-    }), Some(8), size.1/2+5);
+    centred_print(
+        stdout,
+        &format!(
+            "Select column: {}",
+            match board.highlighted_column {
+                Some(column) => format!("{}", (column + 1).to_string().yellow()),
+                None => "".to_string(),
+            }
+        ),
+        Some(8),
+        size.1 / 2 + 5,
+    );
     // If a column is selected, print the "Press enter to confirm" message
     match board.highlighted_column {
-        Some(_) => centred_print(stdout, &format!("{}", "Press enter to confirm."), None, size.1/2+6),
+        Some(_) => centred_print(
+            stdout,
+            &format!("{}", "Press enter to confirm."),
+            None,
+            size.1 / 2 + 6,
+        ),
         None => (),
     }
-    centred_print(stdout, &format!("{}{}", "Press Ctrl+C to quit at any time.".dimmed(), termion::cursor::Goto(0, size.1)), Some(16), size.1/2+8);
+    centred_print(
+        stdout,
+        &format!(
+            "{}{}",
+            "Press Ctrl+C to quit at any time.".dimmed(),
+            termion::cursor::Goto(0, size.1)
+        ),
+        Some(16),
+        size.1 / 2 + 8,
+    );
 }
